fdfc4b5d430b28cd30e60a496b2a5ea9eee6825c
Ratnesh kumar tiwari <retneshtiwari@gmail.com>
2024-09-28 12:24:31 +0530

    files upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Card from './components/Card/Card.jsx'
+import Grid from './components/Grid/Grid.jsx';
+
+function App() {
+  
+  return (
+    <>
+      < Grid numberOfCards={9} />
+    </>
+  )
+}
+
+export default App;
